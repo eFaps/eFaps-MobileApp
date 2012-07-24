595be7d99e57bdac0910e900ff5be09b85a254f0
jan@moxter.net <jan@moxter.net@fee104cc-1dfa-8c0f-632d-d3b7e6b59fb0>
2012-07-24 02:35:52 +0000

    - adding session, request management to mobile

--- eFaps-MobileApp/trunk/src/main/java/org/efaps/mobile/wicket/MobileApplication.java
+++ eFaps-MobileApp/trunk/src/main/java/org/efaps/mobile/wicket/MobileApplication.java
@@ -18,19 +18,25 @@
  * Last Changed By: $Author$
  */
 
-
 package org.efaps.mobile.wicket;
 
 import org.apache.wicket.Page;
+import org.apache.wicket.Session;
+import org.apache.wicket.authorization.strategies.page.SimplePageAuthorizationStrategy;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.protocol.http.WebSession;
+import org.apache.wicket.request.Request;
+import org.apache.wicket.request.Response;
+import org.efaps.mobile.wicket.pages.ISecuredPage;
 import org.efaps.mobile.wicket.pages.login.LoginPage;
-
+import org.efaps.mobile.wicket.request.MobileRequestCycleListener;
 
 /**
  * TODO comment!
  *
  * @author The eFaps Team
- * @version $Id$
+ * @version $Id: MobileApplication.java 7821 2012-07-23 23:40:46Z jan@moxter.net
+ *          $
  */
 public class MobileApplication
     extends WebApplication
@@ -42,4 +48,42 @@ public class MobileApplication
         return LoginPage.class;
     }
 
+
+    /**
+     * @see org.apache.wicket.protocol.http.WebApplication#newSession(org.apache.wicket.Request,
+     *      org.apache.wicket.Response)
+     * @param _request the request
+     * @param _response the response
+     * @return a new Session for the request
+     */
+    @Override
+    public Session newSession(final Request _request,
+                              final Response _response)
+    {
+        return new MobileSession(_request);
+    }
+
+
+
+    @Override
+    protected void init()
+    {
+        getMarkupSettings().setStripWicketTags(true);
+        getMarkupSettings().setStripComments(true);
+        getMarkupSettings().setCompressWhitespace(true);
+        getMarkupSettings().setAutomaticLinking(false);
+
+        getRequestCycleListeners().add(new MobileRequestCycleListener());
+
+        getSecuritySettings().setAuthorizationStrategy(
+                        new SimplePageAuthorizationStrategy(ISecuredPage.class, LoginPage.class)
+                        {
+                            @Override
+                            protected boolean isAuthorized()
+                            {
+                                final MobileSession session = (MobileSession) WebSession.get();
+                                return session.isAuthenticated();
+                            }
+                        });
+    }
 }
